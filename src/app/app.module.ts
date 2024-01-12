4feefaf117fbe21e0fb9304dac1e8056235838bc
VILA Loris <loris.vila@sncf.fr>
2024-01-12 09:38:46 +0100

    feat : initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { WcsAngularModule } from "wcs-angular";
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidemenuComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WcsAngularModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
